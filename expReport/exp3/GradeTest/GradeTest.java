ee016037b6d10db81465737341f90bf0fe1532c3
Wenjie Huang <knsugit@outlook.com>
2018-10-22 13:13:18 +0800

    实验最后一题未完成

--- expReport/exp3/GradeTest/GradeTest.java
+++ expReport/exp3/GradeTest/GradeTest.java
@@ -1,9 +1,20 @@
 class Grade{
-    public double grade;
+    public double StuGrade;
+    public double TeaGrade;
+    public double CouGrade;
+    void setGrade(double grade){}
+    double getGrade(){
+        return 0;
+    }
 }
 
 class Student extends Grade{
-
+    void setGrade(double grade){
+        super.StuGrade = grade;
+    }
+    double getGrade(){
+        return super.StuGrade;
+    }
 }
 
 class Teacher extends Grade{
@@ -11,5 +22,13 @@ class Teacher extends Grade{
 }
 
 class Course extends Grade{
-    
+
+}
+
+public class GradeTest{
+    public static void main(String args[]){
+        Student stu = new Student();
+        stu.setGrade(10);
+        System.out.println(stu.getGrade());
+    }
 }
