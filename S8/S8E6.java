3cac295a424dedf87cdfcc09e23e3cc249963da2
WenjieHuang <knsugit@outlook.com>
2018-11-16 16:31:48 +0800

    使用了Timer的版本

--- S8/S8E6.java
+++ S8/S8E6.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.text.*;
 
-
 public class S8E6 {
     public static void main(String[] args) {
         Timer t1 = new Timer();
@@ -19,31 +18,34 @@ public class S8E6 {
 }
 
 class PekingTimerTask extends TimerTask {
-    public synchronized void run() {
-        Date date = new Date();
-        SimpleDateFormat bjSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
-        bjSdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
-        System.out.print("Peking:\t" + bjSdf.format(date) + "\r");
-        //System.out.println("");
+    public void run() {
+        synchronized(this) {
+            Date date = new Date();
+            SimpleDateFormat bjSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+            bjSdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+            System.out.println("Peking:\t" + bjSdf.format(date) + "\r");
+        }
     }
 }
 
 class TokyoTimerTask extends TimerTask {
-    public synchronized void run() {
-        Date date = new Date();
-        SimpleDateFormat tokyoSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        tokyoSdf.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
-        System.out.print("Tokyo:\t" + tokyoSdf.format(date) + "\r");
-        //System.out.println("");
+    public void run() {
+        synchronized(this) {
+            Date date = new Date();
+            SimpleDateFormat tokyoSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            tokyoSdf.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
+            System.out.println("Tokyo:\t" + tokyoSdf.format(date) + "\r");
+        }
     }
 }
 
 class LondonTimerTask extends TimerTask {
-    public synchronized void run() {
-        Date date = new Date();
-        SimpleDateFormat londonSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        londonSdf.setTimeZone(TimeZone.getTimeZone("Europe/London"));
-        System.out.print("London:\t" + londonSdf.format(date) + "\r");
-        //System.out.println("");
+    public void run() {
+        synchronized(this) {
+            Date date = new Date();
+            SimpleDateFormat londonSdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            londonSdf.setTimeZone(TimeZone.getTimeZone("Europe/London"));
+            System.out.println("London:\t" + londonSdf.format(date) + "\r");
+        }
     }
 }
